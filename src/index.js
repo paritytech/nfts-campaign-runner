99d3129f9160db0352fbb7a1bce6df8ce27d4e6a
hamidra <hamid.alipour@gmail.com>
2021-11-30 20:00:26 +0100

    add cli commander

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const { runWorkflow } = require('./workflow');
+#!/usr/bin/env node
 
-runWorkflow()
+const { Command } = require('commander');
+const { runWorkflow } = require('./workflow');
+const program = new Command();
+
+program.version('0.0.1');
+
+program
+  .argument(`<workflow-config>`, 'the workflow configuration file')
+  .action(async (workflowConfig) => {
+    runWorkflow(workflowConfig);
+    console.log('done!');
+  });
+
+program
+  .parseAsync(process.argv)
   .then(() => process.exit(0))
   .catch((err) => {
     console.log(err);
